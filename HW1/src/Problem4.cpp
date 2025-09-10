993227483ddf7804b40f551f78d64ff9ea400142
covalent <mail@lukehmcc.com>
2025-09-10 10:50:52 -0400

    add gitignore

---
+++ HW1/src/Problem4.cpp
@@ -0,0 +1,7 @@
+#include "Problem4.h"
+#include <iostream>
+
+bool isJumbled(char *str1, char *str2) {
+  std::cout << str1[5] << std::endl;
+  return false;
+}
--- HW1/src/main.cpp
+++ HW1/src/main.cpp
@@ -1,5 +1,11 @@
+#include "Problem4.h"
 #include <iostream>
+
 int main() {
-  std::cout << "Hi" << std::endl;
+  char str1[] = "abcd";
+  char str2[] = "cbad";
+  const bool isJum = isJumbled(str1, str2);
+  std::cout << "String 1: " << str1 << ", String 2: " << str2
+            << ", is isJumbled: " << (isJum ? "true" : "false") << std::endl;
   return 0;
 }
