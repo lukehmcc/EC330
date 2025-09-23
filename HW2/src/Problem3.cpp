3c23595f2b26019cb4c4e8088d7de481fdc8d8b0
covalent <mail@lukehmcc.com>
2025-09-23 13:21:17 -0400

    finish problem 3 hw 2

--- HW2/src/Problem3.cpp
+++ HW2/src/Problem3.cpp
@@ -1,2 +1,30 @@
+// Problem3.cpp
+// Author: Luke McCarthy
 #include "Problem3.h"
+using namespace std;
 
+vector<int> checkBalanced(vector<string> dataset) {
+  vector<int> toReturn(dataset.size());
+  for (int i = 0; i < dataset.size(); i++) {
+    int orderAcc = 0;
+    int outOfOrder = 0;
+    for (int j = 0; j < dataset.at(i).size(); j++) {
+      if (dataset.at(i).at(j) == '(') {
+        orderAcc++;
+      } else if (dataset.at(i).at(j) == ')') {
+        if (orderAcc <= 0) {
+          outOfOrder++;
+        } else {
+          orderAcc--;
+        }
+      }
+    }
+    if ((orderAcc == 0 && outOfOrder == 0) ||
+        (orderAcc == 1 && outOfOrder == 1)) {
+      toReturn.at(i) = 1;
+    } else {
+      toReturn.at(i) = 0;
+    }
+  }
+  return toReturn;
+}
--- HW2/src/Problem3.h
+++ HW2/src/Problem3.h
@@ -1,5 +1,9 @@
 #ifndef PROBLEM3_H
 #define PROBLEM3_H
+#include <string>
+#include <vector>
+using namespace std;
 
+vector<int> checkBalanced(vector<string> dataset);
 
 #endif // PROBLEM3_H
