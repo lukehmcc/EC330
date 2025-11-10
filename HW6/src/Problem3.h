1d5596b39fb084b1b06b111bbfc1089599a3860b
covalent <mail@lukehmcc.com>
2025-11-09 19:03:34 -0500

    HW6: begin

---
+++ HW6/src/Problem3.h
@@ -0,0 +1,15 @@
+//
+//  Problem3.h
+//  ec330_hw6
+//
+#include <string>
+using namespace std;
+
+#ifndef Problem4_h
+#define Problem4_h
+
+int threeA(string fileName);
+int threeB(string fileName);
+int threeC(string fileName);
+
+#endif
